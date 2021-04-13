2b05289a9e6adad76cbc2c3f68ea7f348570f60f
Elvis Dourado <elvisdourado@gmail.com>
2021-04-12 22:21:09 -0300

    fim

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b) =>{
+      return a - b
+    })
+    this.length =  this.items.length
+  }
+  get(pos) {
+    if (!this.items[pos]) throw new error('OutOfBounds')
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if(!this.items.length) throw new Error('EmptySortedList');
+    return this.items[this.length -1];
+  }
 
-  min() {}
+  min() {
+    if(!this.items.length) throw new Error('EmptySortedList');
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    if(!this.items.length) { return 0}
+    else {
+      return this.items.reduce((a,b) => (a - b), 0)};
 
-  avg() {}
+  }
+
+  avg() {
+    if (!this.length) {
+      throw new Error("EmptySortedList")
+    }
+    else return (this.sum() / this.length);
+  } 
 }
 
 module.exports = SortedList;
